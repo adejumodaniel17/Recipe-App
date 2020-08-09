14167523f4e9acac23f23f7ba358442bd69c2d6c
Daniel O. Adejumo <adejumodaniel17@gmail.com>
2020-08-09 23:09:30 +0100

    Updates

---
+++ precache-manifest.4b300103c5ec2c9e607298b573891ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee539aaf8eb6b97e2205190622b911a",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "5bb454900cb70bbeadeb",
+    "url": "/Recipe-App/static/css/main.6da4e89f.chunk.css"
+  },
+  {
+    "revision": "fd9402947ab48cbd0564",
+    "url": "/Recipe-App/static/js/2.db80dac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.db80dac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb454900cb70bbeadeb",
+    "url": "/Recipe-App/static/js/main.b032e093.chunk.js"
+  },
+  {
+    "revision": "c8b39b8dc776b7362745",
+    "url": "/Recipe-App/static/js/runtime-main.bddd3bf1.js"
+  }
+]);
